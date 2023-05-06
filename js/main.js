5a78babcffb0b65a4916f8ed7773e7231b343ed5
JacekMwo <jckbanach@gmail.com>
2023-05-06 09:30:55 +0200

    29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Deklaracja zmiennych firstNumber i secondNumber oraz przypisanie do nich wartości
+let firstNumber = 24;
+let secondNumber = 8;
+
+function sumaLiczb(number1, number2){
+let suma = (number1 + number2);
+return suma;
+}
+
+let wynik = sumaLiczb(firstNumber, secondNumber);
+console.log(wynik);
